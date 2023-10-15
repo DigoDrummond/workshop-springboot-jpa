0c78bd9d5347f95239df4128fa6709a6ee13f8d9
Rodrigo Drummond Macedo <rodrigodm2810@gmail.com>
2023-10-14 22:40:59 -0300

    JPA repository, dependency injection, database seeding

---
+++ WebServiceSpringJPA (1)/WebServiceSpringJPA/src/main/java/com/rodrigoDrummond/WebServiceSpringJPA/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.rodrigoDrummond.WebServiceSpringJPA.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.rodrigoDrummond.WebServiceSpringJPA.entities.User;
+
+//interface que herda métodos da interface JPARepository
+//<tipo da entidade, tipo atributo chave>
+public interface UserRepository extends JpaRepository<User, Long>{
+
+}
