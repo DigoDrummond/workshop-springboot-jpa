d835704155700e95868be221a623a0ec0439570f
Rodrigo Drummond Macedo <rodrigodm2810@gmail.com>
2023-10-14 21:47:18 -0300

    Spring Initializr

---
+++ WebServiceSpringJPA (1)/WebServiceSpringJPA/src/main/java/com/rodrigoDrummond/WebServiceSpringJPA/WebServiceSpringJpaApplication.java
@@ -0,0 +1,13 @@
+package com.rodrigoDrummond.WebServiceSpringJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServiceSpringJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServiceSpringJpaApplication.class, args);
+	}
+
+}
---
+++ WebServiceSpringJPA (1)/WebServiceSpringJPA/src/test/java/com/rodrigoDrummond/WebServiceSpringJPA/WebServiceSpringJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodrigoDrummond.WebServiceSpringJPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServiceSpringJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
